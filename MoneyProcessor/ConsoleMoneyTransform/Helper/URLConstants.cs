bb1e3c33ae98d80d95d7112ee9adba4417c02a7f
ALieskov <a.leskoff@gmail.com>
2015-03-07 00:32:56 +0200

    w commit fix

---
+++ MoneyProcessor/ConsoleMoneyTransform/Helper/URLConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMoneyTransform.Helper
+{
+    public class URLConstants
+    {
+        public static string MinfinSell = "http://minfin.com.ua/currency/auction/usd/sell/kiev/";
+        public static string MinfinBuy  = "http://minfin.com.ua/currency/auction/usd/buy/kiev/";
+    }
+}
